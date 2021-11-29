4b2767aabcc2c55daf3cc644a76f9cf72bc983d7
ChrisRolling <chrisrolling27@gmail.com>
2021-11-29 11:38:55 -0800

    basic grocery

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// TODO
+
+var GroceryList = () => (
+  <div> Groceries needed:
+  <Olive_Oil />
+  <Bacon />
+  </div>
+);
+
+var Olive_Oil = () => (
+  <div> Olive Oil here
+  </div>
+);
+
+
+var Bacon = () => (
+  <div> Bacon
+  </div>
+);
+
+
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
